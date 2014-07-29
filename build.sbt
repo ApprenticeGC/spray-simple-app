5b9f0fe1659f8c9ca3f1205fd4c28bbb8fd57885
Apprentice <apprentice@giantcroissant.com>
2014-07-30 07:47:52 +0800

    Add json support
    Use spray-json to parse the http method enity in a very rough way.

--- build.sbt
+++ build.sbt
@@ -8,10 +8,16 @@ resolvers += "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositor
 
 resolvers += "spray repo" at "http://repo.spray.io"
 
+resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.3.4",
   "io.spray" %% "spray-can" % "1.3.1",
   "io.spray" %% "spray-routing" % "1.3.1",
+  "io.spray" %% "spray-http" % "1.3.1",
+  "io.spray" %% "spray-httpx" % "1.3.1",
+  "io.spray" %%  "spray-json" % "1.2.6",
+  "com.typesafe.play" %% "play-json" % "2.3.1",
   "org.reactivemongo" %% "reactivemongo" % "0.11.0-SNAPSHOT",
   "org.reactivemongo" %% "play2-reactivemongo" % "0.11.0-SNAPSHOT"
 )
--- src/main/scala/MyServer.scala
+++ src/main/scala/MyServer.scala
@@ -3,6 +3,23 @@ package com.giantcroissant
 import akka.actor.ActorSystem
 import spray.routing.SimpleRoutingApp
 
+import spray.http._
+import spray.httpx.unmarshalling._
+import spray.httpx.SprayJsonSupport._
+import spray.routing._
+
+import play.api.libs.json._
+
+import spray.json._
+
+import DefaultJsonProtocol._
+
+case class User(name: String = "")
+
+object User extends DefaultJsonProtocol {// with SprayJsonSupport {
+  implicit val format = jsonFormat1(apply)
+}
+
 object Main extends App with SimpleRoutingApp {
   implicit val system = ActorSystem("my-system")
 
@@ -14,8 +31,10 @@ object Main extends App with SimpleRoutingApp {
         }
       } ~
       post {
-        complete {
-          """{ "results": [ { "id": "rer903", "name", "Ray" } ] }"""
+        entity(as[User]) { user =>
+          complete {
+            """{ "id": 9d0fkd, "name": """ + user.name + """ }"""
+          }
         }
       }
     }
