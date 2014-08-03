a6ced2acda9f306cc1bdd243da98284aa2aeeebd
Ray Wang <apprentice@giantcroissant.com>
2014-08-03 16:06:32 +0800

    Replace spry-json with play-json
    The usage is still experimental. Knowing play json's reads, writes
    does help a lot though.

--- src/main/scala/MyServer.scala
+++ src/main/scala/MyServer.scala
@@ -4,24 +4,30 @@ import akka.actor.ActorSystem
 import spray.routing.SimpleRoutingApp
 
 import spray.http._
+import spray.httpx.PlayJsonSupport._
 import spray.httpx.unmarshalling._
-import spray.httpx.SprayJsonSupport._
+import spray.httpx.marshalling._
 import spray.routing._
 
 import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
-import spray.json._
-
-import DefaultJsonProtocol._
-
-case class User(name: String = "")
-
-object User extends DefaultJsonProtocol {// with SprayJsonSupport {
-  implicit val format = jsonFormat1(apply)
-}
+case class User(
+  name: String = "",
+  email: String = ""
+)
 
 object Main extends App with SimpleRoutingApp {
   implicit val system = ActorSystem("my-system")
+
+  implicit val userReads: Reads[User] = (
+    (JsPath \ "name").read[String] and
+    (JsPath \ "email").read[String]
+  )(User.apply _)
+  
+  //implicit val userReads = Json.reads[User]
+
+  def unmarshaller: Unmarshaller[User] = playJsonUnmarshaller[User]
 
   startServer(interface = "localhost", port = 8080) {
     path("users") {
@@ -33,7 +39,11 @@ object Main extends App with SimpleRoutingApp {
       post {
         entity(as[User]) { user =>
           complete {
-            """{ "id": 9d0fkd, "name": """ + user.name + """ }"""
+            s"""{
+              id: 9d0fkd,
+              name: ${user.name},
+              email: ${user.email}
+            }"""
           }
         }
       }
