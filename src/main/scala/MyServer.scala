d8530d1d348d67d8ea4576556c5ba7f66b225409
Ray Wang <apprentice@giantcroissant.com>
2014-08-03 22:19:36 +0800

    Add db code
    Connect the parts to make the routing request to db work. The code
    needs to be cleaned later.

--- src/main/scala/MyServer.scala
+++ src/main/scala/MyServer.scala
@@ -12,12 +12,18 @@ import spray.routing._
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
+import reactivemongo.api._
+
+import play.modules.reactivemongo.json.collection.JSONCollection
+
 case class User(
   name: String = "",
   email: String = ""
 )
 
 object Main extends App with SimpleRoutingApp {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
   implicit val system = ActorSystem("my-system")
 
   implicit val userReads: Reads[User] = (
@@ -29,6 +35,18 @@ object Main extends App with SimpleRoutingApp {
 
   def unmarshaller: Unmarshaller[User] = playJsonUnmarshaller[User]
 
+  def connectToDB() = {
+    import reactivemongo.api._
+    //import scala.concurrent.ExecutionContext.Implicits.global
+
+    val driver = new MongoDriver
+    val connection = driver.connection(List("localhost"))
+
+    val db = connection("mq2")
+
+    db
+  }
+
   startServer(interface = "localhost", port = 8080) {
     path("users") {
       get {
@@ -39,11 +57,30 @@ object Main extends App with SimpleRoutingApp {
       post {
         entity(as[User]) { user =>
           complete {
+            //user
             s"""{
               id: 9d0fkd,
               name: ${user.name},
               email: ${user.email}
             }"""
+          }
+        }
+      }
+    } ~
+    path("projects") {
+      get {
+        complete {
+          """{ "results": [ { "name", "Small World" } ] }"""
+        }
+      } ~
+      post {
+        entity(as[JsObject]) { project =>
+          complete {
+            val db = connectToDB()
+            def collection: JSONCollection = db.collection[JSONCollection]("projects")
+
+            collection.insert(project)
+            project
           }
         }
       }
